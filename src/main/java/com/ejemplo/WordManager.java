491a1b701aa7583f060bfa77d958b682c7f0d769
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-13 12:06:57 -0500

    commit - WordManager .

---
+++ src/main/java/com/ejemplo/WordManager.java
@@ -0,0 +1,39 @@
+package com.ejemplo;
+
+import org.apache.poi.xwpf.usermodel.*;
+import java.io.*;
+
+public class WordManager {
+
+    public void crearWord(String ruta) throws Exception {
+
+        XWPFDocument documento = new XWPFDocument();
+        XWPFParagraph p = documento.createParagraph();
+
+        XWPFRun run = p.createRun();
+
+        run.setBold(true);
+        run.setFontSize(16);
+        run.setText("Proyecto Java Word");
+
+        run.addBreak();
+
+        run.setBold(false);
+        run.setText("Linea numero 1");
+
+        run.addBreak();
+
+        run.setText("Linea numero 2");
+
+        run.addBreak();
+
+        run.setText("Linea numero 3");
+
+        FileOutputStream salida =
+                new FileOutputStream(ruta);
+
+        documento.write(salida);
+        salida.close();
+        documento.close();
+    }   
+}
